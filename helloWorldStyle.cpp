f646cad08c708b414ad660f60a3828494344caf0
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-14 22:14:41 +0000

    Created program with style in c++

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Marcus Wehbi All rights reserved.
+// .
+// Created by: Marcus Wehbi
+// Date: Sep. 14, 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ and Python are very different!" << std::endl;
+    std::cout << "But, they are both great!" << std::endl;
+}
